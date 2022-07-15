9300f0039b051cb93a64fc796bf17ca19071626f
rezcode <rezharians@gmail.com>
2022-07-15 20:18:03 +0800

    fix branch

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import DetailRecipe from "./pages/detailRecipe/DetailRecipe";
 import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
+import Login from "./pages/Login/Login";
 import Register from "./pages/register/Register";
 import WithoutNav from "./components/navbarDisplay/WithoutNav";
 import WithNav from "./components/navbarDisplay/WithNav";
